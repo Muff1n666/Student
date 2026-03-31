10f2c639a4c07a0551986f5ddd1242dfbd56de1f
Muffin <laphsindmitr456@gmail.com>
2026-03-31 19:36:59 +0300

    changed api

--- run_api.py
+++ run_api.py
@@ -17,6 +17,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://student-lovat-alpha.vercel.app",
+        "https://intimal-hymnologic-sachiko.ngrok-free.dev",
     ],
     allow_credentials=True,
     allow_methods=["*"],
